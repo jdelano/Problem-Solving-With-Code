452e77ed3043be76788e0a58c26f80cb013cd669
John Delano <jdelano@cedarville.edu>
2024-12-08 12:59:15 -0500

    Chapter 1 Complete

---
+++ Problem Solving With Code/Program.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+using System;
+
+// Create a variable totalResources and set it to 0.
+int totalResources = 0;
+
+// Prompt user for the number of resources collected.
+Console.Write("Enter number of resources collected: ");
+int collected = int.Parse(Console.ReadLine());
+
+// Add the collected resources to totalResources.
+totalResources += collected;
+
+// Print out the totalResources.
+Console.WriteLine($"Total Resources: {totalResources}");
